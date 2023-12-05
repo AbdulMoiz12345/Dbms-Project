80e8d570ac2d3077015caa84c45d4b540f0438a6
unknown <amoiz.bese22seecs@seecs.edu.pk>
2023-12-05 16:04:12 +0500

    lun par charhe shahzaib

--- Backend/server.js
+++ Backend/server.js
@@ -388,7 +388,7 @@ app.get('/pdfass/:AssignmentID', (req, res) => {
 });
 
 // Assignment collection
-  
+  //Moiz and sons
 app.get('/pdf-listgetass', (req, res) => {
   const AssignmentID = req.query.AssignmentID;
   const sql = 'SELECT SubmissionID FROM assignmentsubmission WHERE AssignmentID =(?)';
